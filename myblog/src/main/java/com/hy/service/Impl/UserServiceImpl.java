6d301d48e66276344feed60e01a7d292634fedbb
C3006248 <1979866043@qq.com>
2021-11-29 19:54:51 +0800

    清理文件

--- myblog/src/main/java/com/hy/service/Impl/UserServiceImpl.java
+++ myblog/src/main/java/com/hy/service/Impl/UserServiceImpl.java
@@ -20,7 +20,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userDao.findByUsernameAndPassword(username, MD5Utils.code(password));
+//        User user = userDao.findByUsernameAndPassword(username, MD5Utils.code(password));
+        User user = userDao.findByUsernameAndPassword(username, password);
         return user;
     }
 }
